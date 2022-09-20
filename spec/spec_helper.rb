bbf0e4f9c8a44e234377792850b99c3cd31d91be
travelingaries <joonhyuk.chang@dramancompany.com>
2022-09-20 15:12:27 +0900

    setup rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,35 @@
+# See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+ENV["RAILS_ENV"] ||= 'test'
+require 'factory_bot'
+require 'webmock/rspec'
+
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+
+RSpec.configure do |config|
+  config.fixture_path = "./fixtures"
+  config.global_fixtures = :all
+
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = false
+
+  # Database cleaner
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+  end
+  config.around(:each, :without_transactions => true) do |ex|
+    DatabaseCleaner.strategy = [:deletion, except: %w[confs promotions]]
+    ex.run
+    DatabaseCleaner.strategy = :transaction
+  end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+  config.append_after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.after(:each) { ActiveSupport::CurrentAttributes.reset_all }
+end
--- test/models/taxi_request_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class TaxiRequestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
