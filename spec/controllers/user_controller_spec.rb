33d07665c6e5697bfd082b4f9bb61a921558197b
travelingaries <joonhyuk.chang@dramancompany.com>
2022-09-20 16:09:27 +0900

    add user sign in test code

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe 'sign_in' do
+    fixtures :users
+    it "success" do
+      post :sign_in, params: { email: "passenger1@dramancompany.com", password: "eiusmod" }
+      expect(response).to have_http_status(200)
+    end
 
+    it "incorrect credentials" do
+      post :sign_in, params: { email: "drama@dramancompany.com", password: "eiusmod" }
+      expect(response).to have_http_status(400)
+    end
+  end
 end
