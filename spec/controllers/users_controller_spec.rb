ee7370cdf893b4436144448391a8838714424346
travelingaries <joonhyuk.chang@dramancompany.com>
2022-09-21 16:50:16 +0900

    유저 로그인 FactoryBot, BCrypt로 동적으로 생성하여 돌리도록 수정

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,16 +1,26 @@
 require 'spec_helper'
 
 RSpec.describe UsersController, type: :controller do
-  describe 'sign_in' do
-    fixtures :users
-    it "success" do
-      post :sign_in, params: { email: "passenger1@dramancompany.com", password: "eiusmod" }
-      expect(response).to have_http_status(200)
-    end
+  password = "password"
 
-    it "incorrect credentials" do
-      post :sign_in, params: { email: "drama@dramancompany.com", password: "eiusmod" }
-      expect(response).to have_http_status(400)
+  describe 'sign_in' do
+    let(:user) { create(:user, password_digest: BCrypt::Password.create(password)) }
+
+    context 'with email' do
+      it "success" do
+        post :sign_in, params: { email: user.email, password: password }
+        expect(response).to have_http_status(200)
+      end
+
+      it "incorrect credentials" do
+        post :sign_in, params: { email: user.email, password: "hi" }
+        expect(response).to have_http_status(400)
+      end
+
+      it "no password provided" do
+        post :sign_in, params: { email: user.email }
+        expect(response).to have_http_status(400)
+      end
     end
   end
 end
