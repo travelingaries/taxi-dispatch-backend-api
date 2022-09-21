e6ba549b1d867ef02963776d9424b727042a1f62
travelingaries <joonhyuk.chang@dramancompany.com>
2022-09-21 09:45:27 +0900

    배차 요청 목록 조회 로직 완성

--- app/controllers/taxi_requests_controller.rb
+++ app/controllers/taxi_requests_controller.rb
@@ -2,10 +2,23 @@ class TaxiRequestsController < ApplicationController
   before_action :authenticate_request
 
   def index
-    case current_user
-    when User::Driver
-    when User::Passenger
+    query = TaxiRequest.order(created_at: :desc)
+    query = query.where(passenger_id: current_user.id) if current_user.is_a?(User::Passenger)
 
+    taxi_requests = []
+    query.find_each(batch_size: 50) do |request|
+      taxi_requests << {
+        id: request.id,
+        address: request.address,
+        driverId: request.driver_id,
+        passengerId: request.passenger_id,
+        status: request.status_lang,
+        acceptedAt: request.accepted_at,
+        createdAt: request.created_at,
+        updatedAt: request.updated_at
+      }
     end
+
+    render json: taxi_requests
   end
 end
--- app/models/taxi_request.rb
+++ app/models/taxi_request.rb
@@ -1,2 +1,14 @@
 class TaxiRequest < ApplicationRecord
+  def status_lang
+    case self.status
+    when 1
+      :waiting
+    when 2
+      :accepted
+    when 3
+      :canceled
+    when 4
+      :completed
+    end
+  end
 end
