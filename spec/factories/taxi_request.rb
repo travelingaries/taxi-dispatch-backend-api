1173e35a8a9ab6d145e1260e795f18c8b358937b
travelingaries <joonhyuk.chang@dramancompany.com>
2022-09-26 15:37:00 +0900

    TaxiRequest 테스트코드 리퀘스트 단위로 리팩토링

--- spec/factories/taxi_request.rb
+++ spec/factories/taxi_request.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     passenger_id      { Faker::Number.digit.to_i }
     driver_id         { Faker::Number.digit.to_i }
     address           { Faker::Address.full_address }
+    status            { 1 }
     created_at        { Time.now - 1.months }
     updated_at        { Time.now - 1.months }
   end
