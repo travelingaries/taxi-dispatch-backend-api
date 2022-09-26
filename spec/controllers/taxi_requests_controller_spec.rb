a4dae7d89c0e6771b7fca7a146c616af90940c12
travelingaries <joonhyuk.chang@dramancompany.com>
2022-09-26 15:37:00 +0900

    TaxiRequest 테스트코드 리퀘스트 단위로 리팩토링

--- spec/controllers/taxi_requests_controller_spec.rb
+++ spec/controllers/taxi_requests_controller_spec.rb
@@ -1,23 +1,29 @@
 require 'spec_helper'
 
 RSpec.describe TaxiRequestsController, type: :controller do
-  describe 'index' do
-    context 'with token' do
-      before(:each) do
-        @new_passenger = create(:passenger)
-        @prev_passenger = create(:passenger)
-      end
+  before(:each) do
+    @address = Faker::Address.full_address[0...100]
+    @exp = 1.day.from_now.to_i
+    @secret_key = Rails.application.secret_key_base
+  end
 
-      it 'passenger' do
-        payload = { user_id: @new_passenger.id, exp: 1.day.from_now.to_i }
-        token =  JWT.encode(payload, Rails.application.secret_key_base)
-        @new_passenger.token = token
-        @new_passenger.save!
+  context 'with passenger token' do
+    before(:each) do
+      @passenger = create(:passenger)
+      payload = { user_id: @passenger.id, exp: @exp }
+      token = JWT.encode(payload, @secret_key)
+      @passenger.token = token
+      @passenger.save!
 
-        @request.headers["Authorization"] = "Token #{token}"
+      @request.headers["Authorization"] = "Token #{token}"
+    end
 
-        new_taxi_request = create(:taxi_request, passenger_id: @new_passenger.id)
-        prev_taxi_request = create(:taxi_request, passenger_id: @prev_passenger.id)
+    describe 'index' do
+      it 'success' do
+        prev_passenger = create(:passenger)
+
+        new_taxi_request = create(:taxi_request, passenger_id: @passenger.id)
+        prev_taxi_request = create(:taxi_request, passenger_id: prev_passenger.id)
 
         get :index
         expect(response).to have_http_status(200)
@@ -25,19 +31,42 @@ RSpec.describe TaxiRequestsController, type: :controller do
         expect(json.is_a?(Array)).to eq(true)
         expect(json.length).to eq(1)
       end
+    end
 
-      it 'driver' do
-        driver = create(:driver)
-        payload = { user_id: driver.id, exp: 1.day.from_now.to_i }
-        token =  JWT.encode(payload, Rails.application.secret_key_base)
-        driver.token = token
-        driver.save!
+    describe 'create' do
+      it 'success' do
+        post :create, params: { address: @address }
+        expect(response).to have_http_status(200)
+      end
 
-        @request.headers["Authorization"] = "Token #{token}"
+      it 'address too long' do
+        post :create, params: { address: 'a' * 101 }
+        expect(response).to have_http_status(400)
+      end
 
-        new_taxi_request = create(:taxi_request, passenger_id: @new_passenger.id)
-        prev_taxi_request = create(:taxi_request, passenger_id: @prev_passenger.id)
+      it 'duplicated' do
+        create(:taxi_request, passenger_id: @passenger.id)
+        post :create, params: { address: @address }
+        expect(response).to have_http_status(409)
+      end
+    end
+  end
 
+  context 'with driver token' do
+    fixtures :taxi_requests
+
+    before(:each) do
+      @driver = create(:driver)
+      payload = { user_id: @driver.id, exp: @exp }
+      token = JWT.encode(payload, @secret_key)
+      @driver.token = token
+      @driver.save!
+
+      @request.headers["Authorization"] = "Token #{token}"
+    end
+
+    describe 'index' do
+      it 'success' do
         get :index
         expect(response).to have_http_status(200)
         json = JSON.parse(response.body)
@@ -46,9 +75,32 @@ RSpec.describe TaxiRequestsController, type: :controller do
       end
     end
 
-    context 'without valid token' do
-      it 'request without valid token' do
+    describe 'create' do
+      it 'cannot create' do
+        post :create, params: { address: @address }
+        expect(response).to have_http_status(403)
+      end
+    end
+  end
+
+  context 'without valid token' do
+    describe 'index' do
+      it 'without token' do
         get :index
+        expect(response).to have_http_status(401)
+      end
+
+      it 'with nonexistent token' do
+        @request.headers["Authorization"] = "Token 1234"
+
+        get :index
+        expect(response).to have_http_status(401)
+      end
+    end
+
+    describe 'create' do
+      it 'without token' do
+        post :create, params: { address: @address }
         expect(response).to have_http_status(401)
       end
     end
