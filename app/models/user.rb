a1607a91b30fb7eaf5284873c594f64a8d25c80b
travelingaries <joonhyuk.chang@dramancompany.com>
2022-09-26 10:03:30 +0900

    TaxiRequest, User 모델에 association 추가

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   include ActiveModel::SecurePassword
   has_secure_password
 
+  if self.attribute(:type) == "User::Driver"
+    has_many :taxi_requests, -> { where(driver_id: self.attribute(:id)) }
+  else
+    has_many :taxi_requests, -> { where(passenger_id: self.attribute(:id)) }
+  end
+
   before_save :downcase_email
 
   def downcase_email
