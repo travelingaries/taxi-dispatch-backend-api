a9fc9424a8fd9bcaced46302a5b5f57453e4af55
Aries <aries@studioaries.co.kr>
2022-09-27 13:42:23 +0900

    TaxiRequest factory에서 passenger 자동 생성 코드 간결하게
    Co-authored-by: kimjinsoo <93517120+soolab@users.noreply.github.com>

--- spec/factories/taxi_request.rb
+++ spec/factories/taxi_request.rb
@@ -1,14 +1,10 @@
 FactoryBot.define do
   factory :taxi_request do
+    passenger
     driver_id         {  }
     address           { Faker::Address.full_address }
     status            { 1 }
     created_at        { Time.now - 1.months }
     updated_at        { Time.now - 1.months }
-
-    before(:create) do |taxi_request|
-      passenger = create(:passenger)
-      taxi_request.passenger = passenger
-    end
   end
 end
