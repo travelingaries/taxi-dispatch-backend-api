aa335799e00a867d18059588913195a4512434da
travelingaries <joonhyuk.chang@dramancompany.com>
2022-09-28 17:37:20 +0900

    TaxiRequestsController 테스트코드 리팩토링

--- spec/controllers/taxi_requests_controller_spec.rb
+++ spec/controllers/taxi_requests_controller_spec.rb
@@ -1,152 +1,206 @@
 require 'spec_helper'
 
 RSpec.describe TaxiRequestsController, type: :controller do
-  before(:each) do
-    @address = Faker::Address.full_address[0...100]
-    @exp = 1.day.from_now.to_i
-    @secret_key = Rails.application.secret_key_base
-  end
+  let(:address) { Faker::Address.full_address[0...100] }
+  let(:exp) { 1.day.from_now.to_i }
+  let(:secret_key) { Rails.application.secret_key_base }
 
-  context 'with passenger token' do
+  subject { JSON.parse(response.body) }
+
+  context '유효한 토큰이 있는 승객인 경우' do
+    let!(:passenger) { create(:passenger) }
+    let(:payload) { { user_id: passenger.id, exp: exp } }
+    let(:token) { JWT.encode(payload, secret_key) }
+
     before(:each) do
-      @passenger = create(:passenger)
-      payload = { user_id: @passenger.id, exp: @exp }
-      token = JWT.encode(payload, @secret_key)
-      @passenger.token = token
-      @passenger.save!
+      passenger.token = token
+      passenger.save!
 
-      @request.headers["Authorization"] = "Token #{token}"
+      @request.headers['Authorization'] = "Token #{token}"
     end
 
-    describe 'index' do
-      it 'success' do
-        prev_passenger = create(:passenger)
-
-        new_taxi_request = create(:taxi_request, passenger_id: @passenger.id)
-        prev_taxi_request = create(:taxi_request, passenger_id: prev_passenger.id)
-
+    describe '#index' do
+      def request
         get :index
-        expect(response).to have_http_status(200)
-        json = JSON.parse(response.body)
-        expect(json.is_a?(Array)).to eq(true)
-        expect(json.length).to eq(1)
+      end
+
+      context '올바른 요청인 경우' do
+        let!(:prev_passenger) { create(:passenger) }
+
+        let!(:new_taxi_request) { create(:taxi_request, passenger_id: passenger.id) }
+        let!(:prev_taxi_request) { create(:taxi_request, passenger_id: prev_passenger.id) }
+
+        it_behaves_like 'OK 응답 처리', :request
+
+        it '응답이 길이 1의 배열이다' do
+          request
+          expect(subject.is_a?(Array)).to eq(true)
+          expect(subject.length).to eq(1)
+        end
       end
     end
 
-    describe 'create' do
-      it 'success' do
-        post :create, params: { address: @address }
-        expect(response).to have_http_status(200)
+    describe '#create' do
+      def request
+        post :create, params: params
       end
 
-      it 'address too long' do
-        post :create, params: { address: 'a' * 101 }
-        expect(response).to have_http_status(400)
+      let(:params) { { address: address } }
+
+      context '올바른 요청인 경우' do
+        it_behaves_like 'OK 응답 처리', :request
       end
 
-      it 'duplicated' do
-        create(:taxi_request, passenger_id: @passenger.id)
-        post :create, params: { address: @address }
-        expect(response).to have_http_status(409)
+      context '주소가 너무 긴 경우' do
+        before { params[:address] = 'a' * 101 }
+
+        it_behaves_like 'Bad Request 응답 처리', :request do
+          let(:message) { '주소는 100자 이하로 입력해주세요' }
+        end
+      end
+
+      context '기존 요청이 있는 경우' do
+        before { create(:taxi_request, passenger_id: passenger.id) }
+
+        it_behaves_like 'Conflict 응답 처리', :request do
+          let(:message) { '아직 대기중인 배차 요청이 있습니다' }
+        end
       end
     end
 
-    describe 'accept_request' do
-      before(:each) do
-        @taxi_request = create(:taxi_request, passenger_id: @passenger.id)
+    describe '#accept_request' do
+      def request
+        post :accept_request, params: { taxi_request_id: taxi_request.id }
       end
 
-      it 'forbidden' do
-        post :accept_request, params: { taxi_request_id: @taxi_request.id }
-        expect(response).to have_http_status(403)
+      let!(:taxi_request) { create(:taxi_request, passenger_id: passenger.id) }
+
+      context '승객이라 요청을 못 하는 경우' do
+        it_behaves_like 'Forbidden 응답 처리', :request do
+          let(:message) { '기사만 배차 요청을 수락할 수 있습니다' }
+        end
       end
     end
   end
 
-  context 'with driver token' do
+  context '유효한 토큰이 있는 기사인 경우' do
     fixtures :taxi_requests
 
+    let!(:driver) { create(:driver) }
+    let(:payload) { { user_id: driver.id, exp: exp } }
+    let(:token) { JWT.encode(payload, secret_key) }
+
     before(:each) do
-      @driver = create(:driver)
-      payload = { user_id: @driver.id, exp: @exp }
-      token = JWT.encode(payload, @secret_key)
-      @driver.token = token
-      @driver.save!
+      driver.token = token
+      driver.save!
 
       @request.headers["Authorization"] = "Token #{token}"
     end
 
-    describe 'index' do
-      it 'success' do
+    describe '#index' do
+      def request
         get :index
-        expect(response).to have_http_status(200)
-        json = JSON.parse(response.body)
-        expect(json.is_a?(Array)).to eq(true)
-        expect(json.length >= 4).to eq(true)
+      end
+
+      context '올바른 요청인 경우' do
+        it_behaves_like 'OK 응답 처리', :request
+
+        it '응답이 길이 4 이상의 배열이다' do
+          request
+          expect(subject.is_a?(Array)).to eq(true)
+          expect(subject.length >= 4).to eq(true)
+        end
       end
     end
 
-    describe 'create' do
-      it 'forbidden' do
-        post :create, params: { address: @address }
-        expect(response).to have_http_status(403)
+    describe '#create' do
+      def request
+        post :create, params: { address: address }
+      end
+
+      context '기사라 요청을 못 하는 경우' do
+        it_behaves_like 'Forbidden 응답 처리', :request do
+          let(:message) { '승객만 배차 요청할 수 있습니다' }
+        end
       end
     end
 
-    describe 'accept_request' do
-      before(:each) do
-        passenger = create(:passenger)
-        @taxi_request = create(:taxi_request, passenger_id: passenger.id)
+    describe '#accept_request' do
+      def request
+        post :accept_request, params: params
       end
 
-      it 'success' do
-        post :accept_request, params: { taxi_request_id: @taxi_request.id }
-        expect(response).to have_http_status(200)
+      let(:params) { { taxi_request_id: taxi_request.id } }
+
+      let!(:passenger) { create(:passenger) }
+      let!(:taxi_request) { create(:taxi_request, passenger_id: passenger.id) }
+
+      context '올바른 요청인 경우' do
+        it_behaves_like 'OK 응답 처리', :request
       end
 
-      it 'not found' do
-        post :accept_request, params: { taxi_request_id: 0 }
-        expect(response).to have_http_status(404)
+      context '존재하지 않는 요청인 경우' do
+        before { params[:taxi_request_id] = 0 }
+
+        it_behaves_like 'Not Found 응답 처리', :request do
+          let(:message) { '존재하지 않는 배차 요청입니다' }
+        end
       end
 
-      it 'already accepted' do
-        another_driver = create(:driver)
-        @taxi_request.driver = another_driver
-        @taxi_request.save!
+      context '이미 수락된 배차 요청인 경우' do
+        let!(:another_driver) { create(:driver) }
 
-        post :accept_request, params: { taxi_request_id: @taxi_request.id }
-        expect(response).to have_http_status(409)
+        before(:each) do
+          taxi_request.driver = another_driver
+          taxi_request.save!
+        end
+
+        it_behaves_like 'Conflict 응답 처리', :request do
+          let(:message) { '수락할 수 없는 배차 요청입니다. 다른 배차 요청을 선택하세요' }
+        end
       end
     end
   end
 
-  context 'without valid token' do
-    describe 'index' do
-      it 'without token' do
+  context '유효한 토큰이 없는 경우' do
+    describe '#index' do
+      def request
         get :index
-        expect(response).to have_http_status(401)
       end
 
-      it 'with nonexistent token' do
-        @request.headers["Authorization"] = "Token 1234"
+      context '토큰이 없는 경우' do
+        it_behaves_like 'Unauthorized 응답 처리', :request
+      end
 
-        get :index
-        expect(response).to have_http_status(401)
+      context '발급된 적 없는 토큰인 경우' do
+        before(:each) do
+          @request.headers["Authorization"] = "Token 1234"
+        end
+
+        it_behaves_like 'Unauthorized 응답 처리', :request
       end
     end
 
-    describe 'create' do
-      it 'without token' do
-        post :create, params: { address: @address }
-        expect(response).to have_http_status(401)
+    describe '#create' do
+      def request
+        post :create, params: { address: address }
+      end
+
+      context '토큰이 없는 경우' do
+        it_behaves_like 'Unauthorized 응답 처리', :request
       end
     end
 
-    describe 'accept_request' do
-      it 'without token' do
-        taxi_request = create(:taxi_request)
+    describe '#accept_request' do
+      def request
         post :accept_request, params: { taxi_request_id: taxi_request.id }
-        expect(response).to have_http_status(401)
+      end
+
+      let!(:passenger) { create(:passenger) }
+      let!(:taxi_request) { create(:taxi_request, passenger_id: passenger.id) }
+
+      context '토큰이 없는 경우' do
+        it_behaves_like 'Unauthorized 응답 처리', :request
       end
     end
   end
