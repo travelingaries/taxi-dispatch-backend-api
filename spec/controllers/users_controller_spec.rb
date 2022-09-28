0710a75f6c886cefb89a0c3be68b069043ec3dd1
travelingaries <joonhyuk.chang@dramancompany.com>
2022-09-28 18:57:03 +0900

    UsersController 테스트코드 리팩토링

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,71 +1,110 @@
 require 'spec_helper'
 
 RSpec.describe UsersController, type: :controller do
-  password = "password"
+  let(:password) { 'password' }
 
-  describe 'sign_up' do
-    context 'with email' do
-      email = "test@email.com"
-      password = "password"
+  subject { JSON.parse(response.body) }
 
-      it "passenger success" do
-        user_type = "passenger"
-        post :sign_up, params: { email: email, password: password, userType: user_type }
-        expect(response).to have_http_status(200)
-        json = JSON.parse(response.body)
-        expect(json['id']).to_not be_nil
+  describe '#sign_up' do
+    def request
+      post :sign_up, params: params
+    end
+
+    let(:email) { 'test@email.com' }
+    let(:params) { { email: email, password: password, userType: 'passenger' } }
+
+    context '올바른 승객 가입 요청인 경우' do
+      it_behaves_like 'OK 응답 처리', :request
+
+      it '응답에 id 정보가 포함된다' do
+        request
+        expect(subject['id']).to_not be_nil
       end
+    end
 
-      it "driver success" do
-        user_type = "driver"
-        post :sign_up, params: { email: email, password: password, userType: user_type }
-        expect(response).to have_http_status(200)
-        json = JSON.parse(response.body)
-        expect(json['id']).to_not be_nil
+    context '올바른 기사 가입 요청인 경우' do
+      before { params[:userType] = 'driver' }
+
+      it_behaves_like 'OK 응답 처리', :request
+
+      it '응답에 id 정보가 포함된다' do
+        request
+        expect(subject['id']).to_not be_nil
       end
+    end
 
-      it "user duplicated" do
-        prev_user = create(:user, password_digest: BCrypt::Password.create(password))
-        post :sign_up, params: { email: prev_user.email, password: password, userType: "passenger" }
-        expect(response).to have_http_status(409)
+    context '이미 가입된 이메일인 경우' do
+      let!(:prev_user) { create(:user, password_digest: BCrypt::Password.create(password)) }
+
+      before { params[:email] = prev_user.email }
+
+      it_behaves_like 'Conflict 응답 처리', :request do
+        let(:message) { '이미 가입된 이메일입니다' }
       end
+    end
 
-      it "without email" do
-        post :sign_up, params: { password: password, userType: "passenger" }
-        expect(response).to have_http_status(400)
+    context '이메일이 유효하지 않은 경우' do
+      context '이메일 항목이 없는 경우' do
+        before { params.except!(:email) }
+
+        it_behaves_like 'Bad Request 응답 처리', :request do
+          let(:message) { '올바른 이메일을 입력해주세요' }
+        end
       end
+    end
 
-      it "without password" do
-        post :sign_up, params: { email: email, userType: "passenger" }
-        expect(response).to have_http_status(400)
+    context '비밀번호가 유효하지 않은 경우' do
+      context '비밀번호 항목이 없는 경우' do
+        before { params.except!(:password) }
+
+        it_behaves_like 'Bad Request 응답 처리', :request do
+          let(:message) { '올바른 비밀번호를 입력해주세요' }
+        end
       end
+    end
 
-      it "without user type" do
-        post :sign_up, params: { email: email, password: password }
-        expect(response).to have_http_status(400)
+    context '유저 타입이 유효하지 않은 경우' do
+      context '유저 타입 항목이 없는 경우' do
+        before { params.except!(:userType) }
+
+        it_behaves_like 'Bad Request 응답 처리', :request do
+          let(:message) { '올바른 유저 타입을 입력해주세요' }
+        end
       end
     end
   end
 
-  describe 'sign_in' do
+  describe '#sign_in' do
+    def request
+      post :sign_in, params: params
+    end
+
     let(:user) { create(:user, password_digest: BCrypt::Password.create(password)) }
 
-    context 'with email' do
-      it "success" do
-        post :sign_in, params: { email: user.email, password: password }
-        expect(response).to have_http_status(200)
-        json = JSON.parse(response.body)
-        expect(json['accessToken']).to_not be_nil
+    context '올바른 요청인 경우' do
+      let(:params) { { email: user.email, password: password } }
+
+      it_behaves_like 'OK 응답 처리', :request
+
+      it '응답이 토큰을 포함한다' do
+        request
+        expect(subject['accessToken']).to_not be_nil
+      end
+    end
+
+    context '비밀번호가 유효하지 않은 경우' do
+      let(:params) { { email: user.email } }
+
+      let(:message) { '아이디와 비밀번호를 확인해주세요' }
+
+      context '비밀번호가 틀린 경우' do
+        before { params[:password] = 'hi' }
+
+        it_behaves_like 'Bad Request 응답 처리', :request
       end
 
-      it "incorrect credentials" do
-        post :sign_in, params: { email: user.email, password: "hi" }
-        expect(response).to have_http_status(400)
-      end
-
-      it "no password provided" do
-        post :sign_in, params: { email: user.email }
-        expect(response).to have_http_status(400)
+      context '비밀번호 항목이 없는 경우' do
+        it_behaves_like 'Bad Request 응답 처리', :request
       end
     end
   end
