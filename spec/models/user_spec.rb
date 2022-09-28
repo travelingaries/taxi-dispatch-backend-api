f760974dc40d51d92932b3780a00a9913df1ca98
travelingaries <joonhyuk.chang@dramancompany.com>
2022-09-28 20:00:23 +0900

    User 모델 테스트코드 정리

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,29 +1,19 @@
 require "spec_helper"
 
 describe User do
-  before(:each) do
-    @user = create(:user)
-  end
+  let!(:user) { create(:user) }
 
-  describe 'password' do
-    context 'has_secure_password' do
-      it 'initial states' do
-        expect(@user.attributes["password"]).to be_nil
-        expect(@user.password).to be_nil
-        expect(@user.password_confirmation).to be_nil
-        expect(@user.password_digest.present?).to eq(true)
-        expect(@user.password_digest.is_a?(String)).to eq(true)
+  describe '#password' do
+    context '비밀번호를 변경하는 경우' do
+      let(:password) { 'password' }
+
+      before(:each) do
+        user.password = password
+        user.save!
       end
 
-      it 'change password' do
-        prev_password_digest = @user.password_digest
-        password = 'password'
-        @user.password = password
-        expect(@user.valid?).to eq(true)
-
-        @user.save!
-        expect(@user.password_digest != prev_password_digest).to eq(true)
-        expect(@user.authenticate(password).present?).to eq(true)
+      it '변경된 비밀번호로 인증 잘 된다' do
+        expect(user.authenticate(password).present?).to eq(true)
       end
     end
   end
