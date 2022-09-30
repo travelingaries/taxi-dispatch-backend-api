331ec932d871359524dae3b04f20af619517d9a8
travelingaries <joonhyuk.chang@dramancompany.com>
2022-09-30 17:09:12 +0900

    UserTypeResolver concern 따로 분리

--- app/controllers/taxi_requests_controller.rb
+++ app/controllers/taxi_requests_controller.rb
@@ -2,6 +2,14 @@
 
 class TaxiRequestsController < ApplicationController
   before_action :authenticate_request
+  before_action only: :create do
+    allow_passengers_only('승객만 배차 요청할 수 있습니다')
+  end
+  before_action only: :accept_request do
+    allow_drivers_only('기사만 배차 요청을 수락할 수 있습니다')
+  end
+
+  include UserTypeResolver
 
   def index
     requests = TaxiRequest.order(created_at: :desc)
@@ -14,8 +22,6 @@ class TaxiRequestsController < ApplicationController
   end
 
   def create
-    raise Exceptions::Forbidden, '승객만 배차 요청할 수 있습니다' if current_user.is_a?(User::Driver)
-
     prev_request = TaxiRequest.where(passenger_id: current_user.id).order(created_at: :desc).first
     raise Exceptions::Conflict, '아직 대기중인 배차 요청이 있습니다' if prev_request.present?
 
@@ -28,8 +34,6 @@ class TaxiRequestsController < ApplicationController
   end
 
   def accept_request
-    raise Exceptions::Forbidden, '기사만 배차 요청을 수락할 수 있습니다' if current_user.is_a?(User::Passenger)
-
     request = TaxiRequest.find_by(id: accept_request_params[:taxi_request_id])
     raise Exceptions::NotFound, '존재하지 않는 배차 요청입니다' if request.blank?
     raise Exceptions::Conflict, '수락할 수 없는 배차 요청입니다. 다른 배차 요청을 선택하세요' if request.driver_id.present?
