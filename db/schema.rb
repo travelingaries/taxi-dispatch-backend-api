2d0d104917db26aefb1228b2dfb3c352839ff730
travelingaries <joonhyuk.chang@dramancompany.com>
2022-10-05 14:06:18 +0900

    rubocop 돌려돌려

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,26 +12,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_19_071817) do
-  create_table "taxi_requests", force: :cascade do |t|
-    t.integer "passenger_id", limit: 11, null: false
-    t.integer "driver_id", limit: 11
-    t.string "address", limit: 255, null: false
-    t.string "status", limit: 50, default: "standBy", null: false
-    t.datetime "accepted_at", precision: nil
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
+ActiveRecord::Schema[7.0].define(version: 20_220_919_071_817) do
+  create_table 'taxi_requests', force: :cascade do |t|
+    t.integer 'passenger_id', limit: 11, null: false
+    t.integer 'driver_id', limit: 11
+    t.string 'address', limit: 255, null: false
+    t.string 'status', limit: 50, default: 'standBy', null: false
+    t.datetime 'accepted_at', precision: nil
+    t.datetime 'created_at', precision: nil, null: false
+    t.datetime 'updated_at', precision: nil, null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "type", limit: 255
-    t.string "email", limit: 255, null: false
-    t.string "password_digest", limit: 255, null: false
-    t.string "token", limit: 255
-    t.string "status", limit: 50, default: "inspecting", null: false
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
-    t.text "data"
+  create_table 'users', force: :cascade do |t|
+    t.string 'type', limit: 255
+    t.string 'email', limit: 255, null: false
+    t.string 'password_digest', limit: 255, null: false
+    t.string 'token', limit: 255
+    t.string 'status', limit: 50, default: 'inspecting', null: false
+    t.datetime 'created_at', precision: nil, null: false
+    t.datetime 'updated_at', precision: nil, null: false
+    t.text 'data'
   end
-
 end
