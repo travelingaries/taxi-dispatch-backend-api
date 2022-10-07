56552f926e91798b7e90ff5a910a90d215167dec
travelingaries <joonhyuk.chang@dramancompany.com>
2022-10-07 11:25:11 +0900

    taxi_requests_controller_spec describe 상위 구조로 변경

--- spec/controllers/taxi_requests_controller_spec.rb
+++ spec/controllers/taxi_requests_controller_spec.rb
@@ -9,56 +9,97 @@ RSpec.describe TaxiRequestsController, type: :controller do
 
   subject { JSON.parse(response.body) }
 
-  context '유효한 토큰이 있는 승객인 경우' do
-    let!(:passenger) { create(:passenger) }
-    let(:payload) { { user_id: passenger.id, exp: exp } }
-    let(:token) { JWT.encode(payload, secret_key) }
-
-    before do
-      passenger.update!(token: token)
-
-      @request.headers['Authorization'] = "Token #{token}"
+  describe '#index' do
+    def request
+      get :index
     end
 
-    describe '#index' do
-      def request
-        get :index
+    context '유효한 토큰이 있는 승객인 경우' do
+      let!(:passenger) { create(:passenger) }
+      let(:payload) { { user_id: passenger.id, exp: exp } }
+      let(:token) { JWT.encode(payload, secret_key) }
+
+      before do
+        passenger.update!(token: token)
+
+        @request.headers['Authorization'] = "Token #{token}"
       end
 
-      context '올바른 요청인 경우' do
-        let!(:another_passenger) { create(:passenger) }
+      let!(:another_passenger) { create(:passenger) }
 
-        let!(:new_taxi_request) { create(:taxi_request, passenger: passenger) }
-        let!(:prev_taxi_request) { create(:taxi_request, passenger: passenger, status: :completed) }
-        let!(:another_taxi_request) { create(:taxi_request, passenger: another_passenger) }
+      let!(:new_taxi_request) { create(:taxi_request, passenger: passenger) }
+      let!(:prev_taxi_request) { create(:taxi_request, passenger: passenger, status: :completed) }
+      let!(:another_taxi_request) { create(:taxi_request, passenger: another_passenger) }
 
-        it_behaves_like 'OK 응답 처리', :request
+      it_behaves_like 'OK 응답 처리', :request
 
-        let(:expected_response) do
-          requests = TaxiRequest.order(id: :desc).where(passenger_id: passenger.id)
-          ActiveModel::Serializer::CollectionSerializer.new(requests, serializer: TaxiRequestSerializer).as_json
-        end
+      let(:expected_response) do
+        requests = TaxiRequest.order(id: :desc).where(passenger_id: passenger.id)
+        ActiveModel::Serializer::CollectionSerializer.new(requests, serializer: TaxiRequestSerializer).as_json
+      end
 
-        it '자신의 배차 요청 목록이 응답된다' do
-          request
+      it '자신의 배차 요청 목록이 응답된다' do
+        request
 
-          expect(subject).to eq(expected_response.as_json)
-        end
+        expect(subject).to eq(expected_response.as_json)
       end
     end
 
-    describe '#create' do
-      def request
-        post :create, params: params
+    context '유요한 토큰이 있는 기사인 경우' do
+      let!(:driver) { create(:driver) }
+      let(:payload) { { user_id: driver.id, exp: exp } }
+      let(:token) { JWT.encode(payload, secret_key) }
+
+      before do
+        driver.update!(token: token)
+
+        @request.headers['Authorization'] = "Token #{token}"
       end
 
-      let(:params) { { address: address } }
+      let!(:taxi_requests) { create_list(:taxi_request, 4) }
+
+      it_behaves_like 'OK 응답 처리', :request
+
+      let(:expected_response) do
+        requests = TaxiRequest.order(id: :desc)
+        ActiveModel::Serializer::CollectionSerializer.new(requests, serializer: TaxiRequestSerializer).as_json
+      end
+
+      it '모든 배차 요청 목록이 응답된다' do
+        request
+
+        expect(subject).to eq(expected_response.as_json)
+      end
+    end
+
+    context '유효한 토큰이 없는 경우' do
+      it_behaves_like 'Unauthorized 응답 처리', :request
+    end
+  end
+
+  describe '#create' do
+    def request
+      post :create, params: params
+    end
+
+    let(:params) { { address: address } }
+
+    context '유효한 토큰이 있는 승객인 경우' do
+      let!(:passenger) { create(:passenger) }
+      let(:payload) { { user_id: passenger.id, exp: exp } }
+      let(:token) { JWT.encode(payload, secret_key) }
+
+      before do
+        passenger.update!(token: token)
+
+        @request.headers['Authorization'] = "Token #{token}"
+      end
 
       context '올바른 요청인 경우' do
         it_behaves_like 'Created 응답 처리', :request
       end
 
-      context '올바르지 않은 요청인 경우' do
+      context '올바르지 못한 요청인 경우' do
         context '주소가 없는 경우' do
           it_behaves_like 'Bad Request 응답 처리', :request do
             let(:params) { {} }
@@ -83,70 +124,62 @@ RSpec.describe TaxiRequestsController, type: :controller do
       end
     end
 
-    describe '#accept_request' do
-      def request
-        post :accept_request, params: { taxi_request_id: taxi_request.id }
+    context '유효한 토큰이 있는 기사인 경우' do
+      let!(:driver) { create(:driver) }
+      let(:payload) { { user_id: driver.id, exp: exp } }
+      let(:token) { JWT.encode(payload, secret_key) }
+
+      before do
+        driver.update!(token: token)
+
+        @request.headers['Authorization'] = "Token #{token}"
       end
 
-      let!(:taxi_request) { create(:taxi_request, passenger: passenger) }
-
-      context '승객이라 요청을 못 하는 경우' do
-        it_behaves_like 'Forbidden 응답 처리', :request do
-          let(:message) { '기사만 배차 요청을 수락할 수 있습니다' }
-        end
+      it_behaves_like 'Forbidden 응답 처리', :request do
+        let(:message) { '승객만 배차 요청할 수 있습니다' }
       end
+    end
+
+    context '유효한 토큰이 없는 경우' do
+      it_behaves_like 'Unauthorized 응답 처리', :request
     end
   end
 
-  context '유효한 토큰이 있는 기사인 경우' do
-    let!(:driver) { create(:driver) }
-    let!(:taxi_requests) { create_list(:taxi_request, 4) }
-    let(:payload) { { user_id: driver.id, exp: exp } }
-    let(:token) { JWT.encode(payload, secret_key) }
+  describe '#accept_request' do
+    let!(:passenger) { create(:passenger) }
+    let!(:taxi_request) { create(:taxi_request, passenger: passenger) }
 
-    before do
-      driver.update!(token: token)
-
-      @request.headers['Authorization'] = "Token #{token}"
+    def request
+      post :accept_request, params: params
     end
 
-    describe '#index' do
-      def request
-        get :index
+    let(:params) { { taxi_request_id: taxi_request.id } }
+
+    context '유효한 토큰이 있는 승객인 경우' do
+      let(:payload) { { user_id: passenger.id, exp: exp } }
+      let(:token) { JWT.encode(payload, secret_key) }
+
+      before do
+        passenger.update!(token: token)
+
+        @request.headers['Authorization'] = "Token #{token}"
       end
 
-      context '올바른 요청인 경우' do
-        it_behaves_like 'OK 응답 처리', :request
-
-        it '응답이 길이 4 이상의 배열이다' do
-          request
-          expect(subject.is_a?(Array)).to eq(true)
-          expect(subject.length >= 4).to eq(true)
-        end
+      it_behaves_like 'Forbidden 응답 처리', :request do
+        let(:message) { '기사만 배차 요청을 수락할 수 있습니다' }
       end
     end
 
-    describe '#create' do
-      def request
-        post :create, params: { address: address }
+    context '유효한 토큰이 있는 기사인 경우' do
+      let!(:driver) { create(:driver) }
+      let(:payload) { { user_id: driver.id, exp: exp } }
+      let(:token) { JWT.encode(payload, secret_key) }
+
+      before do
+        driver.update!(token: token)
+
+        @request.headers['Authorization'] = "Token #{token}"
       end
-
-      context '기사라 요청을 못 하는 경우' do
-        it_behaves_like 'Forbidden 응답 처리', :request do
-          let(:message) { '승객만 배차 요청할 수 있습니다' }
-        end
-      end
-    end
-
-    describe '#accept_request' do
-      def request
-        post :accept_request, params: params
-      end
-
-      let(:params) { { taxi_request_id: taxi_request.id } }
-
-      let!(:passenger) { create(:passenger) }
-      let!(:taxi_request) { create(:taxi_request, passenger: passenger) }
 
       context '올바른 요청인 경우' do
         it_behaves_like 'OK 응답 처리', :request
@@ -177,48 +210,9 @@ RSpec.describe TaxiRequestsController, type: :controller do
         end
       end
     end
-  end
 
-  context '유효한 토큰이 없는 경우' do
-    describe '#index' do
-      def request
-        get :index
-      end
-
-      context '토큰이 없는 경우' do
-        it_behaves_like 'Unauthorized 응답 처리', :request
-      end
-
-      context '발급된 적 없는 토큰인 경우' do
-        before do
-          @request.headers['Authorization'] = 'Token 1234'
-        end
-
-        it_behaves_like 'Unauthorized 응답 처리', :request
-      end
-    end
-
-    describe '#create' do
-      def request
-        post :create, params: { address: address }
-      end
-
-      context '토큰이 없는 경우' do
-        it_behaves_like 'Unauthorized 응답 처리', :request
-      end
-    end
-
-    describe '#accept_request' do
-      def request
-        post :accept_request, params: { taxi_request_id: taxi_request.id }
-      end
-
-      let!(:passenger) { create(:passenger) }
-      let!(:taxi_request) { create(:taxi_request, passenger: passenger) }
-
-      context '토큰이 없는 경우' do
-        it_behaves_like 'Unauthorized 응답 처리', :request
-      end
+    context '토큰이 없는 경우' do
+      it_behaves_like 'Unauthorized 응답 처리', :request
     end
   end
 end
