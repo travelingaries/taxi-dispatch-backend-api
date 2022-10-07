341635785b912f1e157b64929a252db61787484f
travelingaries <joonhyuk.chang@dramancompany.com>
2022-10-07 18:00:35 +0900

    has_secure_password 테스트코드는 불필요해서 제거

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,19 +8,4 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_length_of(:email).is_at_most(100) }
   end
-
-  describe '#password' do
-    context '비밀번호를 변경하는 경우' do
-      let(:password) { 'password' }
-
-      before do
-        user.password = password
-        user.save!
-      end
-
-      it '변경된 비밀번호로 인증 잘 된다' do
-        expect(user.authenticate(password).present?).to eq(true)
-      end
-    end
-  end
 end
